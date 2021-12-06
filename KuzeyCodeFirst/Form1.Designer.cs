73c2e81a13b37686c579eb211036600d3345b78e
Fulya Sarıyar Abdullah <fulya12agustos@hotmail.com>
2021-12-06 14:17:55 +0300

    cf audit test ok.

--- KuzeyCodeFirst/Form1.Designer.cs
+++ KuzeyCodeFirst/Form1.Designer.cs
@@ -28,7 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.Gray;
+            this.btnEkle.Location = new System.Drawing.Point(53, 94);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(147, 23);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.BackColor = System.Drawing.Color.Gray;
+            this.btnGuncelle.Location = new System.Drawing.Point(161, 136);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(147, 23);
+            this.btnGuncelle.TabIndex = 1;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = false;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.Color.Gray;
+            this.btnSil.Location = new System.Drawing.Point(284, 174);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(147, 23);
+            this.btnSil.TabIndex = 2;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = false;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
             // 
             // Form1
             // 
@@ -36,6 +72,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(587, 358);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnEkle);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -43,5 +82,9 @@
         }
 
         #endregion
+
+        private Button btnEkle;
+        private Button btnGuncelle;
+        private Button btnSil;
     }
 }
--- KuzeyCodeFirst/Form1.cs
+++ KuzeyCodeFirst/Form1.cs
@@ -1,3 +1,6 @@
+using KuzeyCodeFirst.Data;
+using KuzeyCodeFirst.Models;
+
 namespace KuzeyCodeFirst
 {
     public partial class Form1 : Form
@@ -5,6 +8,31 @@ namespace KuzeyCodeFirst
         public Form1()
         {
             InitializeComponent();
+        }
+        private KuzeyContext _dbContext = new KuzeyContext();
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            _dbContext.Kategoriler.Add(entity: new Kategori
+            {
+                Ad="Kategori",
+                Aciklama="Açýklama"
+            });
+            _dbContext.SaveChanges();
+            
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            var kategori = _dbContext.Kategoriler.First();
+            kategori.Aciklama = "Güncel Açýklama";
+            _dbContext.SaveChanges();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            var kategori = _dbContext.Kategoriler.First();
+            _dbContext.Kategoriler.Remove(kategori);
+            _dbContext.SaveChanges();
         }
     }
 }
