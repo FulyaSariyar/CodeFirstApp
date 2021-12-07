feae5919a9c12d765a9d88bbd9cbc30ded5d3a05
Fulya Sarıyar Abdullah <fulya12agustos@hotmail.com>
2021-12-07 10:10:36 +0300

    repository 1

---
+++ KuzeyCodeFirst/Repository/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using KuzeyCodeFirst.Models.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuzeyCodeFirst.Repository
+{
+    internal interface IRepository<T,in TId> where T :BaseEntity // in TId,in sayesinde degistiremezsin ama kullanabilirsin.
+    {
+        T GetById(TId id);
+        IQueryable<T> Get(Func<T,bool> predicate=null);
+
+
+
+        void Add(T entity,bool isSaveLater=false); //SaveChange metodunu her kayit ekledigimde calistirmamak icin isSaveLater ekledim.
+        void Remove(T entity, bool isSaveLater = false);
+        void Update(T entity, bool isSaveLater = false);
+        int Save();
+
+
+
+    }
+}
